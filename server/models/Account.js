344e329ec8428b8fdb592c81fdfe1bcc74a17081
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-03-31 00:03:25 +0200

    Added Account model

---
+++ server/models/Account.js
@@ -0,0 +1,56 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+class Account extends Model {
+    getFullName() {
+        return [this.firstname, this.lastname].join(' ')
+    }
+}
+
+Account.init({
+    accountId: {
+        type: DataTypes.INT,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    firstname: {
+        type: DataTypes.String,
+        allowNull: false
+    },
+    lastname: {
+        type: DataTypes.String,
+        allowNull: false
+    },
+    username: {
+        type: DataTypes.String,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.String,
+        allowNull: false
+    },
+    passwordHash: {
+        type: DataTypes.String,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.String,
+        allowNull: false
+    },
+    isArchived: {
+        type: DataTypes.Boolean,
+        defaultValue: false,
+        allowNull: false
+    },
+    resetKey: DataTypes.String,
+    lastConnectedAt: DataTypes.Date
+}, {
+    sequelize,
+    modelName: 'Account'
+})
+
+(async () => {
+    await sequelize.sync();
+    // Code here
+})();
