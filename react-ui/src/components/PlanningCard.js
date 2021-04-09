7e746a4cb4a2eb9c006692c9d501d544b32299a2
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-04-09 16:46:36 +0200

    Replaced class by className

--- react-ui/src/components/PlanningCard.js
+++ react-ui/src/components/PlanningCard.js
@@ -3,7 +3,7 @@ import React from "react";
 export function PlanningCard({godfatherName,laureateName})
 {
     return <div>
-                <div class="col-sm-2 card-header text-center bg-info mt-5">
+                <div className="col-sm-2 card-header text-center bg-info mt-5">
                     <p>  {godfatherName + " / " + laureateName} </p>
                 </div>
     </div>
--- react-ui/src/navigation/Navigation.js
+++ react-ui/src/navigation/Navigation.js
@@ -16,31 +16,31 @@ export class Navigation extends Component   // Permet de créer la barre de navi
                         </li>
 
                         <li className="nav-item d-flex flex-row">
-                        <Link className="nav-link align-self-center"to="/ApplicantList"> <button className="btn btn-dark btn-sm align-self-center" > ApplicantList </button> </Link>
+                        <Link className="nav-link align-self-center" to="/ApplicantList"> <button className="btn btn-dark btn-sm align-self-center" > ApplicantList </button> </Link>
                         </li>
 
                         <li className="nav-item">
-                        <Link className="nav-link"to="/ManageAccounts"> <button className="btn btn-dark btn-sm align-self-center" > Manage Accounts </button> </Link>
+                        <Link className="nav-link" to="/ManageAccounts"> <button className="btn btn-dark btn-sm align-self-center" > Manage Accounts </button> </Link>
                         </li>
 
                         <li className="nav-item">
-                        <Link className="nav-link"to="/Rating"> <button className="btn btn-dark btn-sm align-self-center" > Ratings </button> </Link>
+                        <Link className="nav-link" to="/Rating"> <button className="btn btn-dark btn-sm align-self-center" > Ratings </button> </Link>
                         </li>
 
                         <li className="nav-item">
-                        <Link className="nav-link"to="/ApplicantForm"> <button className="btn btn-dark btn-sm align-self-center" > ApplicantForm </button> </Link>
+                        <Link className="nav-link" to="/ApplicantForm"> <button className="btn btn-dark btn-sm align-self-center" > ApplicantForm </button> </Link>
                         </li>
 
-                        <li class="nav-item">
-                        <Link class="nav-link"to="/Timeline"> <button className="btn btn-dark btn-sm align-self-center" > Timeline </button> </Link>
+                        <li className="nav-item">
+                        <Link className="nav-link" to="/Timeline"> <button className="btn btn-dark btn-sm align-self-center" > Timeline </button> </Link>
                         </li>
 
-                        <li class="nav-item">
-                        <Link class="nav-link"to="/PlanningV2"> <button className="btn btn-dark btn-sm align-self-center" > Planning test </button> </Link>
+                        <li className="nav-item">
+                        <Link className="nav-link" to="/PlanningV2"> <button className="btn btn-dark btn-sm align-self-center" > Planning test </button> </Link>
                         </li>
 
-                        <li class="nav-item">
-                        <Link class="nav-link"to="/APITest"> <button className="btn btn-dark btn-sm align-self-center" > API test </button> </Link>
+                        <li className="nav-item">
+                        <Link className="nav-link" to="/APITest"> <button className="btn btn-dark btn-sm align-self-center" > API test </button> </Link>
                         </li>
                     </ul>
                     </div>
