6fcf7d90e85833602c5c5d98bda768757dfc2416
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-04-14 19:00:31 +0200

    Modified Home page

--- react-ui/src/pages/Home.js
+++ react-ui/src/pages/Home.js
@@ -3,8 +3,11 @@ import {Container} from 'react-bootstrap';
 
 export function Home() {
     return (
-        <Container className='title'>
+        <Container className='title py-4'>
             <h1>Welcome to Emergence App</h1>
+            <div className="text-center py-4">
+                <a href="/Login" className="btn btn-primary">Login</a>
+            </div>
         </Container>
     )
 }
