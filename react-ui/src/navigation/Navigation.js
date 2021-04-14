4deab03b78cca1e62e5e7e8a5d2a4a88054a8501
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-04-14 14:32:01 +0200

    Fixed packages, merge conflicts

--- react-ui/src/navigation/Navigation.js
+++ react-ui/src/navigation/Navigation.js
@@ -8,49 +8,24 @@ export class Navigation extends Component   // Permet de créer la barre de navi
 {
     render()
     {
-        return <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Emergence</Navbar.Brand>
-        <Nav className="mr-auto">
-            
-          <Nav.Link href="/Login">Login</Nav.Link>
-          <Nav.Link href="/ApplicantList">ApplicantList</Nav.Link>
-          <Nav.Link href="/Rating">Rating</Nav.Link>
-          <Nav.Link href="/Timeline">Timeline</Nav.Link>
-          <Nav.Link href="/PlanningV2">PlanningV2</Nav.Link>
-          <Nav.Link href="/APITest">API</Nav.Link>
-          <Nav.Link href="/ApplicantForm">Rating</Nav.Link>
-          <Nav.Link href="/Rating">Rating</Nav.Link>
-          <Nav.Link href="/ManageAccounts">ManageAccounts</Nav.Link>
-         
-        </Nav>
+        return (
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Emergence</Navbar.Brand>
+                <Nav className="mr-auto">
 
-      </Navbar>
+                  <Nav.Link href="/Login">Login</Nav.Link>
+                  <Nav.Link href="/ApplicantList">ApplicantList</Nav.Link>
+                  <Nav.Link href="/Rating">Rating</Nav.Link>
+                  <Nav.Link href="/Timeline">Timeline</Nav.Link>
+                  <Nav.Link href="/PlanningV2">PlanningV2</Nav.Link>
+                  <Nav.Link href="/APITest">API</Nav.Link>
+                  <Nav.Link href="/ApplicantForm">Rating</Nav.Link>
+                  <Nav.Link href="/Rating">Rating</Nav.Link>
+                  <Nav.Link href="/ManageAccounts">ManageAccounts</Nav.Link>
+                  <Nav.Link href="/PrettyPlanning">Joli Planning</Nav.Link>
 
-
-
-
-
-                        <li className="nav-item">
-                        <Link className="nav-link"to="/Timeline"> <button className="btn btn-dark btn-sm align-self-center" > Timeline </button> </Link>
-                        </li>
-
-                        <li className="nav-item">
-                        <Link className="nav-link"to="/PlanningV2"> <button className="btn btn-dark btn-sm align-self-center" > Planning test </button> </Link>
-                        </li>
-
-                        <li className="nav-item">
-                        <Link className="nav-link"to="/APITest"> <button className="btn btn-dark btn-sm align-self-center" > API test </button> </Link>
-                        </li>
-
-                        <li className="nav-item">
-                        <Link className="nav-link"to="/PrettyPlanning"> <button className="btn btn-dark btn-sm align-self-center" > Jolie Planning </button> </Link>
-                        </li>
-                    </ul>
-                    </div>
-                </div>
-                </nav>
-         </div>
-
-
+                </Nav>
+            </Navbar>
+        );
     }
 }
