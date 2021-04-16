25cef00e4a905e561020fef0730aa2c13c4fe682
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-04-17 00:41:10 +0200

    Updated accounts route

--- server/routes/private/accounts.js
+++ server/routes/private/accounts.js
@@ -6,6 +6,7 @@ const accountsController = require('../../controllers/accounts');
 accounts.get('/', accountsController.list);
 accounts.get('/:id([0-9]+)', accountsController.getById);
 accounts.get('/:token([0-9a-z]{128})', accountsController.getByAccessToken);
+accounts.get('/laureates', accountsController.listLaureates);
 
 // POST routes
 accounts.post('/', accountsController.insert);
