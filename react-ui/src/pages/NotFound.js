30b33a69f15072b58675cb3636235f039109566d
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-04-21 00:01:31 +0200

    Added NotFound

---
+++ react-ui/src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NotFound() {
+
+    return (
+        <div className='container py-4'>
+            <h1>Page introuvable - 404</h1>
+            <a className='btn btn-primary' href="/">Retour à l'accueil</a>
+        </div>
+    )
+}
+
+export default NotFound
