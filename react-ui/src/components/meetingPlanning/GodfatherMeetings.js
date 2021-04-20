58023c20c6838a3679a448e8947a17fd2f2a2adb
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-04-20 14:56:33 +0200

    Added MeetingPlanning page, GodfatherMeeting component

---
+++ react-ui/src/components/meetingPlanning/GodfatherMeetings.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function GodfatherMeetings({ godfather }) {
+    return (
+        <>
+            <div className='col-2'>
+                <button>&rsaquo;</button> {godfather}
+            </div>
+            <div className="col-8">
+
+            </div>
+            <div className="col-2">
+
+            </div>
+        </>
+    )
+}
+
+export default GodfatherMeetings
