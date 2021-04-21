9e75d92e63cc1674199f13038d4d9d105612b888
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-04-21 02:34:42 +0200

    Updated AccountInfos component

--- react-ui/src/components/AccountInfos.js
+++ react-ui/src/components/AccountInfos.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-function AccountInfos({ user }) {
+function AccountInfos({ account }) {
     return (
         <div className="row my-4">
             <div className="col col-md-6">
                 <div className="card">
                     <div className="card-header">Mon compte</div>
                     <div className="card-body text-secondary">
-                        <h5 className="card-title">{user.firstname} {user.lastname}</h5>
+                        <h5 className="card-title">{account.firstname} {account.lastname}</h5>
                         <p className="card-text">
-                            {user.email}
-                            {user.phone ? user.phone : null}
+                            {account.email}
+                            {account.phone ? account.phone : null}
                         </p>
                     </div>
                 </div>
