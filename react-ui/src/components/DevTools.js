e3d82095c6a802fc50ee6921ff3abfa751a7dd18
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-04-21 11:36:43 +0200

    Fixed phase selector for development

--- react-ui/src/components/DevTools.js
+++ react-ui/src/components/DevTools.js
@@ -1,12 +1,21 @@
 import React from "react";
+import axios from "axios";
 
 function DevTools({ phase, setPhase }) {
+    function handlePhaseChange(e) {
+        axios.get('//etn-test.herokuapp.com/api/phases/'+ e.target.value)
+            .then((res) => {
+                setPhase(res.data);
+            })
+            .catch((err) => console.log(err));
+    }
+
     return (
         <div className='container-fluid bg-warning'>
             <div className="container py-2">
                 <b className='mr-4'>DevTools</b>
                 <label className='m-0 mr-2' htmlFor='selectPhase'>Phase selector</label>
-                <select className="form-select" id='selectPhase' onChange={(e) => setPhase(e.target.value)}>
+                <select className="form-select" id='selectPhase' onChange={handlePhaseChange}>
                     <option selected={phase === 1} value="1">1</option>
                     <option selected={phase === 2} value="2">2</option>
                     <option selected={phase === 3} value="3">3</option>
