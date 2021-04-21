99723b5898c5561566d929b0d51bbee3b3ec2570
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-04-21 16:29:22 +0200

    Added Empty pages

---
+++ react-ui/src/pages/admin/MeetingsPlanning.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function MeetingsPlanning() {
+
+    return (
+        <div className='container py-4'>Meeting Not implemented</div>
+    )
+}
+
+export default MeetingsPlanning
---
+++ react-ui/src/pages/laureate/MeetingsList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function MeetingsList() {
+
+    return (
+        <div className='container py-4'>Meeting Not implemented</div>
+    )
+}
+
+export default MeetingsList
