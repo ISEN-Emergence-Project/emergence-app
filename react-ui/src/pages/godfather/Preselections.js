5fa31664c997762dbd4f219971b27dcfc0b1cf95
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-04-22 01:57:39 +0200

    Updated Godfather Preselection, laureate number

--- react-ui/src/pages/godfather/Preselections.js
+++ react-ui/src/pages/godfather/Preselections.js
@@ -80,9 +80,9 @@ export function Preselections({ account }) {
                                 </ul>
                             </div>
                             <div className='d-flex flex-nowrap align-items-center ml-auto py-2 '>
-                                <p className='m-0 mr-3 text-secondary text-small'>Sélectionnez 4 lauréats</p>
-                                {(selectedLaureates.length !== 4) ? (
-                                    <button className='btn btn-danger cursor-not-allowed my-2' disabled data-toggle="tooltip" data-placement="top" title="Sélectionnez 4 laureats">Valider</button>
+                                <p className='m-0 mr-3 text-secondary text-small'>Sélectionnez 6 lauréats</p>
+                                {(selectedLaureates.length !== 6) ? (
+                                    <button className='btn btn-danger cursor-not-allowed my-2' disabled data-toggle="tooltip" data-placement="top" title="Sélectionnez 6 laureats">Valider</button>
                                 ) : (
                                     <button className='btn btn-success my-2' type='submit'>Valider</button>
                                 )}
@@ -93,99 +93,4 @@ export function Preselections({ account }) {
             </div>
         );
     }
-}
-
-function Test(props){
-    const [show, setShow] = useState(false);
-  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const[answer,setAnswer] = useState([])
-
-    const[question,setQuestion] = useState([])
-
-
-    useEffect(() => {
-        const options = {
-            method: "GET",
-            header:
-            {
-                'content-type': 'application/json',
-                'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MTgxNDMxNzIsImV4cCI6MTYxODIyOTU3Mn0.5patB5mX43WUUsCHVPnoAbmz-rEnLwyqRLyAJCl_Ss0'
-            }
-        }
-        fetch("https://etn-test.herokuapp.com/api/answers",{options})
-        .then(res => {
-            res.json()
-            .then(res => {
-                return setAnswer(res)
-            })
-            
-        })
-        
-        .catch(error => console.error("There was an error",error)) 
-       
-    },[]);
-
-    useEffect(() => {
-        const options = {
-            method: "GET",
-            header:
-            {
-                'content-type': 'application/json',
-                'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MTgxNDMxNzIsImV4cCI6MTYxODIyOTU3Mn0.5patB5mX43WUUsCHVPnoAbmz-rEnLwyqRLyAJCl_Ss0'
-            }
-        }
-        fetch("https://etn-test.herokuapp.com/api/questions",{options})
-        .then(res => {
-            res.json()
-            .then(res => {
-                return setQuestion(res)
-            })
-        })
-        
-        .catch(error => console.error("There was an error",error)) 
-       
-    },[]);
-    return(
-        <div className='col-6 py-2'>
-        <div  key={props.accountId} >
-            <div className="card">
-                <div className="card-body">
-                    <div className="column d-flex flex-row">
-                        <div onClick={handleShow}>
-                        <div className="col">
-                            <h5>{props.firstname} {props.lastname}</h5>
-                            
-                        </div>
-                        <div className="col">{answer.map(ans=> props.accountId==ans.fkAccountId ?(ans.fkQuestionId==18 ?<div key={ans.answerId}>{ans.answer}</div>:false) : false)}</div>
-                        <div className="col">{answer.map(ans=> props.accountId==ans.fkAccountId ?(ans.fkQuestionId==25 ?<div key={ans.answerId}>{ans.answer}</div>:false) : false)}</div>
-                        </div>   
-                        <div className="col text-right align-self-center">
-                            <input type='checkbox' name={props.accountId} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header>
-                    <Modal.Title> Informations du profil </Modal.Title>
-            </Modal.Header>
-            <Modal.Body className="container">
-            {question.map(ques=>  <div key={ques.questionId}><h5 class="font-weight-bold mb-3" >{ques.question} :</h5> {answer.map(ans=> props.accountId==ans.fkAccountId ?
-            (ans.fkQuestionId==ques.questionId ?
-            <div class ="font-weight-normal" key={ans.answerId}><div class="bg-light border t p-2 mb-2">{ans.answer}</div></div>:false) 
-            : false)}</div>)}
-                
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                Close
-                </Button>
-            </Modal.Footer>
-            </Modal>
-        </div>
-    )
-
 }
