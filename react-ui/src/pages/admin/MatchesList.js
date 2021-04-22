7a995b9ffbea7a737ebc2f378291fd017c459f8b
Nicolas <nicolas.defoort@isen.yncrea.fr>
2021-04-22 16:19:51 +0200

    MatchesList.js resultpage for admin

--- react-ui/src/pages/admin/MatchesList.js
+++ react-ui/src/pages/admin/MatchesList.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios"
 
 function MatchesList() {
 
+    const [matches,setMatches]=useState([])
+    const [account,setAccounts]=useState([])
+    
+
+    axios.get("https://etn-test.herokuapp.com/api/matches")
+            .then(res => {
+            
+            setMatches(res.data)
+
+    })
+    .catch(error => console.error("There was an error",error)) 
+
+
+    axios.get("https://etn-test.herokuapp.com/api/accounts")
+            .then(res => {
+            
+            setAccounts(res.data)
+
+    })
+    .catch(error => console.error("There was an error",error)) 
+
     return (
-        <div className='container py-4'>Meeting Not implemented</div>
+        
+        <div className='container py-4 '>
+            <h1 className="p-2 m-5">Binomes finaux : </h1>
+            {matches.map(matches=> 
+            <div className="d-flex flex-row border-bottom border-top p-2">
+                <h5 className="col">
+                    {account.map(acc=>acc.accountId==matches.fkGodfatherAccountId?
+                    <div className="p-2">{acc.firstname + " "+ acc.lastname}</div>
+                    :false)}
+                </h5>
+                <h5 className="col">
+                    {account.map(acc=>acc.accountId==matches.fkLaureateAccountId?
+                        <div className="p-2">{acc.firstname + " "+ acc.lastname}</div>
+                        :false)}
+                </h5>
+            </div>)}
+        </div>
     )
 }
 
